60ef578d5288d49c438e2d9b89dc35f521ef82a4
Rocco Bowling <rocco@chimerasw.com>
2022-02-28 22:58:35 -0500

    adding in ask() functionality

--- dist/private/figurehead/ui/ui.alert.js
+++ dist/private/figurehead/ui/ui.alert.js
@@ -1,8 +1,16 @@
 #define PAMPHLET_PREPROCESSOR
 
+#include <../utility/defines.js>
+
 var alertsQueue = [];
 
 function handleAlertsQueue() {
+    
+    let animateAlert = function(div, t) {
+	    div.style.height = (t * div.actualHeight) + "px";
+        div.style.minHeight = div.style.height;
+    }
+    
     var openContainer = function() {
         if (alertsContainer.isOpen == false) {
             alertsContainer.isOpen = true;
@@ -24,13 +32,12 @@ function handleAlertsQueue() {
     }
     var closeAlert = function (uniqueID) {
         var alertDiv = document.getElementById(uniqueID);
-        if (alertDiv != undefined) {
+        if EXISTS(alertDiv) {
             if (alertsQueue.length == 0) {
                 closeContainer();
             }
         	Laba.animate(alertDiv, "!f0", function(div, t) {
-        	    div.style.height = ((1.0 - t) * div.actualHeight) + "px";
-                div.style.minHeight = div.style.height;
+                animateAlert(div, 1.0 - t);
         	}, function() {
         	    removeFromParent(alertDiv);
                 handleAlertsQueue();
@@ -39,6 +46,8 @@ function handleAlertsQueue() {
     }
     
     var openAlert = function (alert) {
+        var askValue = alert.value;
+        var ask = alert.ask;
         var message = alert.message;
         var buttons = alert.buttons;
         var callbacks = alert.callbacks;
@@ -55,15 +64,43 @@ function handleAlertsQueue() {
                 buttonsHtml += `ALERT_BUTTON_CONTROL(${uniqueID}Btn${idx},${btnName})`;
             }
         }
-        var alertHtml = `ALERT(${uniqueID},${message},${buttonsHtml})`;
+        var alertHtml = "";
+        
+        if (ask) {
+            alertHtml = `ASK(${uniqueID},${message},${buttonsHtml})`;
+        } else {
+            alertHtml = `ALERT(${uniqueID},${message},${buttonsHtml})`;
+        }
+        
         insertHtml(alertsContainer, alertHtml);
-    
+        
+        var input = document.getElementById(uniqueID + "Value");
+        if EXISTS(input) {
+            input.callback = callbacks[callbacks.length-1];
+            textFieldOnEnter(input, function() {
+                let value = undefined;
+                if EXISTS(input) {
+                    value = input.value;
+                }
+                if EXISTS(input.callback) {
+                    input.callback(value);
+                }
+                closeAlert(uniqueID);
+            });
+            input.value = askValue;
+            input.focus();
+        }
+        
         for (var idx = callbacks.length-1; idx >= 0; idx -= 1) {
             var button0 = document.getElementById(uniqueID + "Btn" + idx);
             button0.callback = callbacks[idx];
             button0.addEventListener("mouseup", function(evt) {
-                if (evt.currentTarget.callback != undefined) {
-                    evt.currentTarget.callback();
+                let value = undefined;
+                if EXISTS(input) {
+                    value = input.value;
+                }
+                if EXISTS(evt.currentTarget.callback) {
+                    evt.currentTarget.callback(value);
                 }
                 closeAlert(uniqueID);
             });
@@ -74,8 +111,7 @@ function handleAlertsQueue() {
         
             alertDiv.actualHeight = alertDiv.offsetHeight;
         	Laba.animate(alertDiv, "!f1", function(div, t) {
-        	    div.style.height = (t * div.actualHeight) + "px";
-                div.style.minHeight = div.style.height;
+        	    animateAlert(div, t);
         	}, function(div) {
                 div.style.height = "";
         	});
@@ -95,22 +131,54 @@ function handleAlertsQueue() {
 }
 
 function alert(message, buttons, callbacks) {
-    if (message == undefined || message.length == 0) {
+    if UNDEFINED(message) {
         return;
     }
     
-    if (alertsContainer.isOpen == undefined) {
+    if UNDEFINED(alertsContainer.isOpen) {
         alertsContainer.isOpen = false;
     }
     
-    if (buttons == undefined) {
+    if UNDEFINED(buttons) {
         buttons = ["Ok"];
     }
-    if (callbacks == undefined) {
+    if UNDEFINED(callbacks) {
         callbacks = [undefined];
     }
     
+    message = message.replaceAll("\n", "<br>");
+    
     alertsQueue.push({
+        message:message,
+        buttons:buttons,
+        callbacks:callbacks,
+    })
+    
+    handleAlertsQueue();
+}
+
+
+function ask(message, value, buttons, callbacks) {
+    if UNDEFINED(message) {
+        return;
+    }
+    
+    if UNDEFINED(alertsContainer.isOpen) {
+        alertsContainer.isOpen = false;
+    }
+    
+    if UNDEFINED(buttons) {
+        buttons = ["Ok"];
+    }
+    if UNDEFINED(callbacks) {
+        callbacks = [undefined];
+    }
+    
+    message = message.replaceAll("\n", "<br>");
+    
+    alertsQueue.push({
+        value: value,
+        ask:true,
         message:message,
         buttons:buttons,
         callbacks:callbacks,
--- dist/private/figurehead/utility/utility.js
+++ dist/private/figurehead/utility/utility.js
@@ -216,6 +216,15 @@ function textFieldOnChange(div, callback) {
     });
 }
 
+function textFieldOnEnter(div, callback) {
+    div.addEventListener("keyup", function(event) {
+    	if (event.keyCode === 13) {
+    		event.preventDefault();
+    		callback();
+    	}
+    });
+}
+
 function UUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
